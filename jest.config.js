19cc54a13b6affbae447cd5dc2607fbe80fcca47
Pierre de la Martinière <pierre.de.la.martiniere@gmail.com>
2018-11-20 17:25:42 +0100

    Fix tests

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,21 @@
 module.exports = {
-  "transform": {
-    "^.+\\.(tsx|ts)?$": "ts-jest"
+  transform: {
+    '^.+\\.(t|j)sx?$': 'ts-jest'
   },
-  "testRegex": "(/__tests__/.*|(\\.|/)(test|spec))\\.(ts|tsx)?$",
-  "moduleFileExtensions": [
-    "ts",
-    "tsx",
-    "js",
-    "jsx",
-    "json",
-    "node"
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
+  moduleFileExtensions: [
+    'ts',
+    'tsx',
+    'js',
+    'jsx',
+    'json',
+    'node'
   ],
-  testURL: 'http://localhost'
+  globals: {
+    'ts-jest': {
+      tsConfig: {
+        jsx: 'react'
+      }
+    }
+  }
 }
