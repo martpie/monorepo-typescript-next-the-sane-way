d7c65e1ca38109df4261421bb48c79b0c77aa01b
Pierre de la Martinière <pierre.de.la.martiniere@gmail.com>
2019-02-11 11:22:52 +0100

    Update to Next.js 8

--- website/next.config.js
+++ website/next.config.js
@@ -1,5 +1,7 @@
 const withTypescript = require('@zeit/next-typescript');
-const withTM = require('next-plugin-transpile-modules');
+// const withTM = require('next-plugin-transpile-modules');
+
+const withTM = require('../../next-plugin-transpile-modules/index');
 
 module.exports = withTypescript(
   withTM({
